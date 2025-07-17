3ad22a2e043f623255202ceff27fa958a12038a3
SDNATH-Git <sd.nath1952@gmail.com>
2025-07-18 01:29:55 +0600

    added vercel

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ function verifyJWT(req, res, next) {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
     console.log("✅ Connected to MongoDB");
 
     const db = client.db("foodSharingDB");
